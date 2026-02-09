9545ccff9ef41d8da4cbdab09c25a9f40a0ea7a9
mribrahimkhan360 <mribrahimkhan360@gmail.com>
2026-02-09 11:17:24 +0600

    dashboard bootstrap skeleton

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,26 @@
-<h1>Dashboard</h1>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<div class="d-flex" id="wrapper">
+    <div class="bg-dark text-white p-3" id="sidebar-wrapper" style="min-width: 200px;min-height: 100vh">
+        <h4 class="text-center">Dashboard</h4>
+    </div>
+    <div class="flex-grow-1" id="page-content-wrapper">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+            <div class="container-fluid">
+                <button type="btn" class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
+                <h5 class="ms-3 mb-0">Welcome to Dashboard</h5>
+            </div>
+        </nav>
+    </div>
+</div>
+</body>
+</html>
